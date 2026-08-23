75c9d1cb4f751fe300cce1c207ac9c078a66d90c
Ajay kumar <127469765+itzaj0001@users.noreply.github.com>
2026-08-23 20:29:38 +0530

    Time: 4 ms (71.15%) | Memory: 21 MB (83.88%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,14 @@
-from math import ceil
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n = len(nums)
-        max = ceil(n/2)
-        hashmap ={}
-
+        count = 0
+        number = 0
         for i in nums:
-            hashmap[i] = hashmap.get(i,0)+1
-
-        for k,v in hashmap.items():
-            if v>=max:
-                return k
+            if count==0:
+                number = i 
+            if i == number:
+                count+=1
+            else:
+                count-=1
+        return number
         
