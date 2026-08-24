67ef10ae501de7885cce258aa0944fd3a1098831
Ajay kumar <127469765+itzaj0001@users.noreply.github.com>
2026-08-25 01:08:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (80.68%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -3,13 +3,10 @@ class Solution:
         lst = []
         for i in operations:
             if i =="+":
-                if len(lst)>=2:
                     lst.append(lst[-1]+lst[-2])
             elif i=="C":
-                if len(lst)>=1:
                     lst.pop()
             elif i=="D":
-                if len(lst)>=1:
                     lst.append(2*lst[-1])
             else:
                 lst.append(int(i))
