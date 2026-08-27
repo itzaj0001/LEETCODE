2a136c4cfdec9b10fbd42e03312cafe6e546e51b
Ajay kumar <127469765+itzaj0001@users.noreply.github.com>
2026-08-27 23:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.80%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,13 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        mydict = {}
-        for i in t:
-            mydict[i] = mydict.get(i,0)+1
-        for i in s:
-            mydict[i] = mydict.get(i,0)-1
-        for k,v in mydict.items():
-            if v==1:
-                return k
+        return chr(sum(ord(c) for c in t) - sum(ord(c) for c in s))
 
 
 
