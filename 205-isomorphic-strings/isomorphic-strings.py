b322e185937fecc1c519d55ba830d561b8d1deab
Ajay kumar <127469765+itzaj0001@users.noreply.github.com>
2026-09-21 15:00:30 +0530

    Time: 4 ms (61.78%) | Memory: 19 MB (97.86%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        d1 = dict()
+        d2 = dict()
+
+        for i, j in zip(s,t):
+            if i not in d1:
+                d1[i] = j
+            if j not in d2:
+                d2[j] = i
+
+            if d1[i] != j or d2[j] != i:
+                    return False
+        return True
+
+
+
+        
