ec6f2e46d66f78a296048aa95c94fbf0df28a96f
Ajay kumar <127469765+itzaj0001@users.noreply.github.com>
2026-09-24 19:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (43.56%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,15 +1,16 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         n = len(arr)
+
         if k == n:
             return arr
 
-        res = []
         l  = 0 
         r = n - 1
 
         while l < r:
-            m = l + (r -l)//2
+            m = l + (r - l) // 2
+
             if arr[m] >= x:
                 r = m                 
             else:
@@ -17,32 +18,23 @@ class Solution:
 
         l = r - 1
 
-        while k > 0:
-            if l == -1:
+        while k:
+            if l < 0:
                 r +=1
-            elif r == n:
+            elif r >= n:
                 l-=1
+            elif x - arr[l] <= arr[r] - x:
+                l -= 1
             else:
-                if abs(arr[l]-x) < abs(arr[r]-x):
-                    l-=1
-                elif abs(arr[l]-x) == abs(arr[r]-x):
-                    if arr[l] < arr[r]:
-                        l-=1
-                    else:
-                        r+=1
-                else:
-                    r+=1
+                r+=1
+
             k-=1
-        
-        while l+1 < r:
-            res.append(arr[l+1])
-            l+=1
-        return res
 
+        return arr[l+1:r]
         
-
-
+    
         
+  
         
 
         
